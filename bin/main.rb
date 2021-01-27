ab37ef92cb21ed7d8103f1a1b6b15adf51d42b10
Jebitok <sharonkosgei4@gmail.com>
2021-01-27 18:45:46 +0300

    add main file

---
+++ bin/main.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require_relative '../lib/check.rb'
+
+check = CheckErrors.new('./bad.rb')
+check.check_identation
+check.trailing_space
+check.tag_error
+check.end_error
+check.empty_line_error
+
+if check.errors.empty? && check.check.err_message.empty?
+    puts 'No offenses'.colorize(:green) + 'detected'
+else
+    check.errors.uniq.each do |err|
+        puts "#{check.check.file_path.colorize(:blue)} : #{err.colorize(:red)}"
+    end
+end
+
+puts check.check.err_message if check.check.file_lines.empty?
